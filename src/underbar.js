60151e45a549fc34c24f4ab3b6dea8a5c3c6ca9e
tyler <datadazer@gmail.com>
2014-10-31 19:56:52 -0700

    _.each now iterates over objects as well as arrays. _.indexof works and reviewed. _.filter passes collection and test as arguments. _.filter returns evens and odds.

--- src/underbar.js
+++ src/underbar.js
@@ -56,8 +56,15 @@ var _ = {};
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator){
+    if(Object.prototype.toString.call(collection) === '[object Array]'){
       for(var i = 0; i < collection.length; i++){
         iterator(collection[i],i,collection);
+      }
+    }
+    else{
+      for(var prop in collection){
+        iterator(collection[prop],prop,collection);
+      }
     }
   };
   // Returns the index at which value can be found in the array, or -1 if value
@@ -79,6 +86,13 @@ var _ = {};
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var testResults = [];
+    _.each(collection,function (itemTested){
+      if (test(itemTested)){
+        testResults.push(itemTested);
+      }
+    });
+    return testResults;
   };
 
   // Return all elements of an array that don't pass a truth test.
